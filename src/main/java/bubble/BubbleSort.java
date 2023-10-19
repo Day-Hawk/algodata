1f8cedb8945fa526d5bc45c3d3f8d5819acffaac
Day-Hawk <daniel.riethmueller@gmx.net>
2023-10-19 12:40:45 +0200

    dev: Added illegal state exception.

--- src/main/java/bubble/BubbleSort.java
+++ src/main/java/bubble/BubbleSort.java
@@ -2,40 +2,44 @@ package bubble;
 
 import java.util.function.BiPredicate;
 
+
 public record BubbleSort<ENTITY>(ENTITY[] list,
-                                 BiPredicate<ENTITY, ENTITY> testLeft) {
+                                 BiPredicate<ENTITY, ENTITY> testLeft
+) {
 
-    public ENTITY[] sort() {
-        return this.sort(this.list.length);
+  public ENTITY[] sort() {
+    return this.sort(this.list.length);
+  }
+
+  private ENTITY[] sort(final int searchUntil) {
+    //Throw error if max length -> illegal state
+    if (searchUntil>this.list.length) {
+      throw new IllegalArgumentException(
+          "Value of searchUntil="+searchUntil+" is out of range="+this.list.length+".");
     }
 
-    private ENTITY[] sort(final int searchUntil) {
-        int currentIndex = 0;
+    int currentIndex = 0;
 
-        if (searchUntil > this.list.length) {
-            return this.list;
-        }
+    //Loop until done
+    while (currentIndex<searchUntil-1) {
+      final ENTITY base = this.list[currentIndex];
+      final ENTITY toCompare = this.list[currentIndex+1];
 
-        //Loop until done
-        while (currentIndex < searchUntil - 1) {
-            final ENTITY base = this.list[currentIndex];
-            final ENTITY toCompare = this.list[currentIndex + 1];
+      //Change position
+      if (this.testLeft.test(base, toCompare)) {
+        this.list[currentIndex] = toCompare;
+        this.list[currentIndex+1] = base;
+      }
 
-            //Change position
-            if (this.testLeft.test(base, toCompare)) {
-                this.list[currentIndex] = toCompare;
-                this.list[currentIndex + 1] = base;
-            }
-
-            currentIndex++;
-        }
-
-        //Goto next position.
-        final int next = searchUntil - 1;
-        if (next > 0) {
-            this.sort(next);
-        }
-
-        return this.list;
+      currentIndex++;
     }
+
+    //Goto next position.
+    final int next = searchUntil-1;
+    if (next>0) {
+      this.sort(next);
+    }
+
+    return this.list;
+  }
 }
